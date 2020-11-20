1d7713704092da46ba0afe917944a207e0fe21be
[rarobin98] <[rarobin98@vt.edu]>
2020-11-20 13:05:38 -0500

    initial

---
+++ 3 Project 2/DNAdbase.java
@@ -0,0 +1,46 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+// -- Ren Robinson (rarobin98)
+
+/**
+ * @author Ren Robinson (rarobin98)
+ * @version 2020.11.20
+ *
+ */
+public class DNAdbase {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    public static long sfold(String s, int M) {
+        int intLength = s.length() / 4;
+        long sum = 0;
+        for (int j = 0; j < intLength; j++) {
+            char c[] = s.substring(j * 4, (j * 4) + 4).toCharArray();
+            long mult = 1;
+            for (int k = 0; k < c.length; k++) {
+                sum += c[k] * mult;
+                mult *= 256;
+            }
+        }
+
+        char c[] = s.substring(intLength * 4).toCharArray();
+        long mult = 1;
+        for (int k = 0; k < c.length; k++) {
+            sum += c[k] * mult;
+            mult *= 256;
+        }
+
+        sum = (sum * sum) >> 8;
+        return (Math.abs(sum) % M);
+    }
+}
